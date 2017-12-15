3eec3d30dffca9a052b3dd152e8b460b675d5ffb
Barbara Sanser <bsanser@gmail.com>
2017-12-15 11:57:30 +0100

    feat: first commit

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,42 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = "UP";
+    this.passengers = [];
+    this.waitingList = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() { 
+    var upd = setInterval(update,1000);
+  }
+  stop() { 
+    clearInterval(upd);
+  }
+  update() {
+    for( let request of requests ){
+      
+    }
+    log();
+   }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  floorUp() { 
+    if(this.floor < this.MAXFLOOR){
+      this.floor++;
+    }
+  }
+  floorDown() { 
+    if(this.floor > 0){
+      this.floor--;
+    }
+  }
+  call(person) { 
+    this.waitingList.push(person);
+    this.requests.push(person.originFloor);
+  }
+  log() { 
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,17 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+var elevator = new Elevator();
+
+var names = ['A','B','C','D','E'];
+var persons = [];
+
+for( let name of names ){
+    let origin = Math.floor(Math.random()*11);
+    let destination = Math.floor(Math.random()*11);
+    if (origin == destination){
+        destination = Math.floor(Math.random()*11);
+    }
+   var pers = new Person(name,origin,destination);
+    persons.push(pers);
+}
